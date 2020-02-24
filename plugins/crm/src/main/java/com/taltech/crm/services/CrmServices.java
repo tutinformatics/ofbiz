b24de15af142a7546c015cfc9875d9fa618e0ada
Siret Jorro <siretjorro@gmail.com>
2020-02-24 22:57:57 +0200

    added not working get contacts service

--- plugins/crm/src/main/java/com/taltech/crm/services/CrmServices.java
+++ plugins/crm/src/main/java/com/taltech/crm/services/CrmServices.java
@@ -1,4 +1,5 @@
 package com.taltech.crm.services;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ofbiz.base.util.Debug;
@@ -25,6 +26,23 @@ public class CrmServices {
             contact = delegator.create(contact);
             result.put("ofbizDemoId", contact.getString("contactId"));
             Debug.log("==========This is my first Java Service implementation in Apache OFBiz. Contact record created successfully with contactId:"+contact.getString("contactId"));
+        } catch (GenericEntityException e) {
+            Debug.logError(e, module);
+            return ServiceUtil.returnError("Error in creating record in OfbizDemo entity ........" +module);
+        }
+        return result;
+    }
+
+    // this doesn't work, returns empty json for some reason
+    public static Map<String, Object> getContacts(DispatchContext dctx, Map<String, ? extends Object> context) {
+        Map<String, Object> result = ServiceUtil.returnSuccess();
+        Delegator delegator = dctx.getDelegator();
+        try {
+            result.put("tere", "tere");
+            List<GenericValue> contact = delegator.findAll("Contact", false);
+//            Debug.log("listi sisu oli " + ofbizDemo.toString());
+//            result.put("list", ofbizDemo.toString());
+//            Debug.log("==========This is my first Java Service implementation in Apache OFBiz. OfbizDemo record created successfully with ofbizDemoId:"+ofbizDemo.getString("ofbizDemoId"));
         } catch (GenericEntityException e) {
             Debug.logError(e, module);
             return ServiceUtil.returnError("Error in creating record in OfbizDemo entity ........" +module);
