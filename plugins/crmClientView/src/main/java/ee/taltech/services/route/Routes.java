9ecc5e375fe13820f978652b062b149f3db50440
Vladislav <Vladislav.Ogorodnik@ttu.ee>
2020-03-01 15:02:26 +0200

    Add missing files

---
+++ plugins/crmClientView/src/main/java/ee/taltech/services/route/Routes.java
@@ -0,0 +1,33 @@
+package main.java.ee.taltech.services.route;
+
+import main.java.ee.taltech.services.ContactsListService;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.apache.ofbiz.service.LocalDispatcher;
+
+public class Routes extends RouteBuilder {
+    LocalDispatcher localDispatcher;
+
+    private ContactsListService contactsListService;
+
+    public Routes(LocalDispatcher localDispatcher) {
+        this.localDispatcher =git  localDispatcher;
+        contactsListService = new ContactsListService(this.localDispatcher.getDispatchContext());
+    }
+
+    @Override
+    public void configure() throws Exception {
+        restConfiguration("rest-api")
+                .component("spark-rest")
+                .host("localhost")
+                .port(7463)
+                .bindingMode(RestBindingMode.json);
+
+        rest("/api")
+                .get("/contact")
+                .produces("application/json")
+                .route()
+                .bean(contactsListService, "getContactList()")
+                .endRest();
+    }
+}
