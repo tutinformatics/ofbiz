e8ef1442f7ca8755f5f71c30b6bd66bcb1ca5f33
envomp <envomp@ttu.ee>
2020-03-08 22:48:50 +0200

    BD - database in citus

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 docker-compose -p ofbiz -f docker-compose-databases.yml up -d
-docker-compose -p ofbiz scale worker=5
+docker-compose -p ofbiz -f docker-compose-databases.yml scale worker=5
+docker-compose -f docker-compose-databases.yml restart master
 docker-compose -p ofbiz -f docker-compose-ofbiz.yml up -d
