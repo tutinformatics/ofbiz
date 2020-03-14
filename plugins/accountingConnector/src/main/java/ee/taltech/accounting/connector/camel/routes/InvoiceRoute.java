84e9559d2d4757def4d2dfc2e5f330e7bd0681e1
eghunt <eghunt@ttu.ee>
2020-03-14 19:56:52 +0200

    Added party and product apis.

--- plugins/accountingConnector/src/main/java/ee/taltech/accounting/connector/camel/routes/InvoiceRoute.java
+++ plugins/accountingConnector/src/main/java/ee/taltech/accounting/connector/camel/routes/InvoiceRoute.java
@@ -1,16 +1,22 @@
 package ee.taltech.accounting.connector.camel.routes;
 
 import ee.taltech.accounting.connector.camel.service.InvoiceService;
+import ee.taltech.accounting.connector.camel.service.PartyService;
+import ee.taltech.accounting.connector.camel.service.ProductService;
 import org.apache.ofbiz.service.LocalDispatcher;
 
 public class InvoiceRoute extends BaseRoute {
 
     private InvoiceService invoiceService;
+    private PartyService partyService;
+    private ProductService productService;
 
 
     public InvoiceRoute(LocalDispatcher localDispatcher) {
         super(localDispatcher);
         invoiceService = new InvoiceService(localDispatcher.getDelegator());
+        partyService = new PartyService(localDispatcher.getDelegator());
+        productService = new ProductService(localDispatcher.getDelegator());
     }
 
     @Override
@@ -37,6 +43,24 @@ public class InvoiceRoute extends BaseRoute {
                 .produces("application/json")
                 .route()
                 .bean(invoiceService, "createInvoice")
+                .endRest();
+
+        rest("/api")
+                .get("/parties")
+                .enableCORS(true)
+                .id("parties-get")
+                .produces("application/json")
+                .route()
+                .bean(partyService, "getParties")
+                .endRest();
+
+        rest("/api")
+                .get("/products")
+                .enableCORS(true)
+                .id("product-get")
+                .produces("application/json")
+                .route()
+                .bean(productService, "getProducts")
                 .endRest();
     }
 }
