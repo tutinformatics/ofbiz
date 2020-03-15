1e30c4d26763eee6c5233bcc57b9e2cf6f19041b
karljoosepkarp <karljk@ttu.ee>
2020-03-15 23:20:54 +0200

    Robert Altmäe endpoints

--- plugins/objectdistribution/src/main/java/ee/ttu/objectdistribution/services/ObjectDistributionServices.java
+++ plugins/objectdistribution/src/main/java/ee/ttu/objectdistribution/services/ObjectDistributionServices.java
@@ -1,12 +1,17 @@
 package ee.ttu.objectdistribution.services;
 
+import com.sun.net.httpserver.HttpServer;
 import org.apache.ofbiz.base.util.Debug;
 import org.apache.ofbiz.entity.Delegator;
 import org.apache.ofbiz.entity.GenericEntityException;
 import org.apache.ofbiz.entity.GenericValue;
 import org.apache.ofbiz.service.DispatchContext;
 import org.apache.ofbiz.service.ServiceUtil;
+import org.json.simple.JSONObject;
 
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
 import java.util.Arrays;
 import java.util.Map;
 
@@ -36,5 +41,26 @@ public class ObjectDistributionServices {
             e.printStackTrace();
         }
         return result;
+    }
+
+    public void getData() throws IOException {
+        int serverPort = 8443;
+        HttpServer server = HttpServer.create(new InetSocketAddress(serverPort), 0);
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("President of the United States", "God-Emperor Trump");
+        server.createContext("/api/hello", (exchange -> {
+            if ("GET".equals(exchange.getRequestMethod())) {
+                String respText = jsonObject.toJSONString();
+                exchange.sendResponseHeaders(200, respText.getBytes().length);
+                OutputStream output = exchange.getResponseBody();
+                output.write(respText.getBytes());
+                output.flush();
+            } else {
+                exchange.sendResponseHeaders(405, -1);// 405 Method Not Allowed
+            }
+            exchange.close();
+        }));
+        server.setExecutor(null); // creates a default executor
+        server.start();
     }
 }
