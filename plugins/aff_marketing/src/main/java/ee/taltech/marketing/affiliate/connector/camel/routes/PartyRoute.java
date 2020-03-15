d8178305ec6d10d6a9ddb642bc1cc454ce9b9b81
Nikita Ojamäe <niojam@ttu.ee>
2020-03-15 14:41:30 +0200

    Create post request

--- plugins/aff_marketing/src/main/java/ee/taltech/marketing/affiliate/connector/camel/routes/PartyRoute.java
+++ plugins/aff_marketing/src/main/java/ee/taltech/marketing/affiliate/connector/camel/routes/PartyRoute.java
@@ -29,7 +29,7 @@ public class PartyRoute extends BaseRoute {
                 .endRest();
 
         rest("/api")
-                .get("/parties/affiliate/create")
+                .post("/parties/affiliate/create")
                 .produces("application/json")
                 .route()
                 .bean(partyService, "createAffiliate")
