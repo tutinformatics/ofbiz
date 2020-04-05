d0b48d23ddd559207530a9551b9aa467648fccc5
Martin Ploom <m.ploom@gmail.com>
2020-04-05 21:10:28 +0300

    Creating new service to add new entries.

---
+++ plugins/marketdata/src/main/java/ee/taltech/marketdata/services/MarketdataService.java
@@ -0,0 +1,10 @@
+package ee.taltech.marketdata.services;
+
+public class MarketdataService {
+
+    public static final String module = MarketdataService.class.getName();
+
+    public static void createMarketdataEntry() {
+
+    }
+}
