758cbbdae61f8a0dc1f8dc43ea3a4f0a39a4d0e1
karlviik <viik.karl2@gmail.com>
2020-04-09 00:44:44 +0300

    BD - Add service POST possibility

--- plugins/ofbiz-jersey-master/src/main/java/org/apache/ofbiz/jersey/resource/ServiceResource.java
+++ plugins/ofbiz-jersey-master/src/main/java/org/apache/ofbiz/jersey/resource/ServiceResource.java
@@ -1,20 +1,23 @@
 package org.apache.ofbiz.jersey.resource;
 
+import org.apache.ofbiz.base.conversion.ConversionException;
+import org.apache.ofbiz.base.lang.JSON;
+import org.apache.ofbiz.base.util.UtilGenerics;
+import org.apache.ofbiz.entity.Delegator;
+import org.apache.ofbiz.entity.util.ExtendedConverters;
 import org.apache.ofbiz.service.DispatchContext;
 import org.apache.ofbiz.service.GenericServiceException;
 import org.apache.ofbiz.service.LocalDispatcher;
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 import java.io.IOException;
+import java.util.Map;
 
 @Path("/v1/services")
 @Provider
@@ -22,6 +25,7 @@ import java.io.IOException;
 public class ServiceResource {
 
 	public static final String MODULE = ServiceResource.class.getName();
+	public static final ExtendedConverters.ExtendedJSONToGenericValue jsonToGenericConverter = new ExtendedConverters.ExtendedJSONToGenericValue();
 
 	@Context
 	private HttpServletRequest httpRequest;
@@ -48,6 +52,37 @@ public class ServiceResource {
 		DispatchContext dpc = dispatcher.getDispatchContext();
 		Object obj = dpc.getModelService(serviceName).getModelParamList();
 		builder = Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(obj);
+		return builder.build();
+	}
+
+	@POST
+	@Path("/{name}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response callService(@PathParam(value = "name") String serviceName, String jsonBody) throws GenericServiceException, IOException {
+		Response.ResponseBuilder builder = null;
+		LocalDispatcher dispatcher = (LocalDispatcher) servletContext.getAttribute("dispatcher");
+		Delegator delegator = (Delegator) servletContext.getAttribute("delegator");
+
+		JSON body = JSON.from(jsonBody);
+
+		Map<String, Object> fieldMap;
+
+		fieldMap = UtilGenerics.<Map<String, Object>>cast(body.toObject(Map.class));
+
+		for (String key : fieldMap.keySet()) {
+			Object obj = fieldMap.get(key);
+			try {
+				Map<String, Object> test = UtilGenerics.<Map<String, Object>>cast(JSON.from(obj).toObject(Map.class));
+				if (test.containsKey("_ENTITY_NAME_")) {
+					fieldMap.put(key, jsonToGenericConverter.convert(delegator.getDelegatorName(), JSON.from(obj)));
+				} else {
+					fieldMap.put(key, test);
+				}
+			} catch (IOException | ConversionException ignored) {
+			}
+		}
+		builder = Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(dispatcher.runSync(serviceName, fieldMap));
 		return builder.build();
 	}
 
