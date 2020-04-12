9ffa0df8530db5b8f6a9abbd5cf8d5111192b7e1
Mihail-PC <mismir@ttu.ee>
2020-04-12 12:45:03 +0300

    apparently, not useless

---
+++ plugins/aff_marketing/src/main/java/ee/taltech/marketing/affiliate/mgr/ProjectTaskCmd.java
@@ -0,0 +1,36 @@
+package ee.taltech.marketing.affiliate.mgr;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+public class ProjectTaskCmd {
+
+    private String workEffortName;
+
+    private String workEffortTypeId;
+
+    private String statusId;
+
+    private String partyId;
+
+    private String workEffortParentId;
+
+    private String currentStatusId;
+
+    private String roleTypeId;
+
+    private Integer estimatedHours;
+
+    private Integer priority;
+
+    private String description;
+
+    //TODO: check how service consumes dates
+    //private LocalDate estimatedStartDate;
+
+    //private LocalDate estimatedCompletionDate;
+}
