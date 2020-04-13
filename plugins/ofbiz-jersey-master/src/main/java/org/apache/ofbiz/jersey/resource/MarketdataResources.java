91dab5910e22157a1d3b77541a55ba99c73b8f00
Martin Ploom <m.ploom@gmail.com>
2020-04-13 10:14:58 +0300

    DELETE method works to delete entry in Marketdata entity based on companyId.

--- plugins/ofbiz-jersey-master/src/main/java/org/apache/ofbiz/jersey/resource/MarketdataResources.java
+++ plugins/ofbiz-jersey-master/src/main/java/org/apache/ofbiz/jersey/resource/MarketdataResources.java
@@ -1,19 +1,11 @@
 package org.apache.ofbiz.jersey.resource;
 
-import org.apache.ofbiz.base.conversion.ConversionException;
-import org.apache.ofbiz.base.lang.JSON;
-import org.apache.ofbiz.entity.Delegator;
-import org.apache.ofbiz.entity.GenericEntity;
-import org.apache.ofbiz.entity.GenericEntityException;
-import org.apache.ofbiz.entity.GenericValue;
-import org.apache.ofbiz.entity.model.ModelEntity;
-import org.apache.ofbiz.entity.model.ModelReader;
+import org.apache.ofbiz.base.util.*;
+import org.apache.ofbiz.entity.*;
+import org.apache.ofbiz.entity.condition.EntityCondition;
+import org.apache.ofbiz.entity.condition.EntityOperator;
 import org.apache.ofbiz.entity.util.ExtendedConverters;
-import org.apache.ofbiz.service.LocalDispatcher;
-import org.apache.ofbiz.service.GenericServiceException;
-import org.apache.ofbiz.base.util.Debug;
-import org.apache.ofbiz.base.util.UtilProperties;
-import org.apache.ofbiz.service.ServiceUtil;
+
 
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
@@ -23,10 +15,8 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.TreeSet;
 
 @Path("/marketdata")
 @Provider
@@ -37,84 +27,83 @@ public class MarketdataResources {
     public static final ExtendedConverters.ExtendedJSONToGenericValue jsonToGenericConverter
             = new ExtendedConverters.ExtendedJSONToGenericValue();
 
-
     @Context
     private HttpServletRequest httpRequest;
 
     @Context
     private ServletContext servletContext;
 
-//    @GET
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public Response getAllMarketdataEntities() throws GenericEntityException {
-//        Response.ResponseBuilder builder = null;
-//        Delegator delegator = (Delegator) servletContext.getAttribute("delegator");
-//        ModelReader reader = delegator.getModelReader();
-//        TreeSet<String> entities = new TreeSet<String>(reader.getEntityNames());
-//        builder = Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(entities);
-//        return builder.build();
-//    }
-
-//    @GET
-//    @Produces(MediaType.APPLICATION_JSON)
-//    public Response getMarketdataList(@QueryParam(value = "projectId") String companyId) {
-//        LocalDispatcher dispatcher = (LocalDispatcher) servletContext.getAttribute("dispatcher");
-//        Map<String, Object> result;
-//
-//        try {
-//            result = dispatcher.runSync("getProjectTaskList",
-//                    Collections.singletonMap("projectId", companyId));
-//        } catch (GenericServiceException e) {
-//            Debug.logError(e, "Exception thrown while running getProjectTaskList service: ", MODULE);
-//            String errMsg = UtilProperties.getMessage("JerseyUiLabels", "api.error.get_task_list",
-//                    httpRequest.getLocale());
-//            throw new RuntimeException(errMsg);
-//        }
-//
-//        if (ServiceUtil.isError(result)) {
-//            ServiceUtil.returnError(ServiceUtil.getErrorMessage(result));
-//        }
-//
-//        return Response
-//                .status(Response.Status.OK)
-//                .type(MediaType.APPLICATION_JSON)
-//                .entity(result)
-//                .build();
-//    }
-
-    // This returns all fields of MarketdataModel entity.
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Response getAllMarketdataEntities() throws GenericEntityException {
         Response.ResponseBuilder builder = null;
         Delegator delegator = (Delegator) servletContext.getAttribute("delegator");
         List<GenericValue> allEntries = delegator.findAll("MarketdataModel", true);
-        //ModelEntity reader = delegator.getModelEntity("MarketdataModel");
-        //List<String> fieldNames = reader.getAllFieldNames();
         builder = Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(allEntries);
         return builder.build();
     }
 
-
-    @POST
-    @Path("/{name}")
+    @DELETE
+    @Path("/{companyId}")
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response addMarketdataEntity(@PathParam(value = "name") String entityName, String jsonBody)
-            throws GenericEntityException, ConversionException {
-        Response.ResponseBuilder builder;
+    public Response deleteEntry(@PathParam(value = "companyId") String companyId) throws GenericEntityException {
+        Response.ResponseBuilder builder = null;
         Delegator delegator = (Delegator) servletContext.getAttribute("delegator");
-        GenericValue object = jsonToGenericConverter.convert(delegator.getDelegatorName(), entityName,
-                JSON.from(jsonBody));
-        ModelEntity model = delegator.getModelEntity(entityName);
-        if (model.getPksSize() == 1) {
-            // because can only sequence if one PK field
-            if (object.get(model.getFirstPkFieldName()) == null) {
-                object.setNextSeqId();
-            }
-        }
-        delegator.create(object); // TODO: catch exception
-        builder = Response.status(Response.Status.OK);
+
+        EntityCondition condition = EntityCondition.makeCondition("companyId", EntityOperator.EQUALS, companyId);
+
+        Map primaryKey = UtilMisc.toMap("companyId", companyId);
+
+        GenericValue result = delegator.findOne("MarketdataModel", primaryKey, false);
+        result.remove();
+
+        builder = Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(result);
         return builder.build();
     }
+
+//    @POST
+//    @Path("/{name}")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response addMarketdataEntity(@PathParam(value = "name") String entityName, String jsonBody)
+//            throws GenericEntityException, ConversionException {
+//        Response.ResponseBuilder builder;
+//        Delegator delegator = (Delegator) servletContext.getAttribute("delegator");
+//        GenericValue object = jsonToGenericConverter.convert(delegator.getDelegatorName(), entityName,
+//                JSON.from(jsonBody));
+//        ModelEntity model = delegator.getModelEntity(entityName);
+//        if (model.getPksSize() == 1) {
+//            // because can only sequence if one PK field
+//            if (object.get(model.getFirstPkFieldName()) == null) {
+//                object.setNextSeqId();
+//            }
+//        }
+//        delegator.create(object); // TODO: catch exception
+//        builder = Response.status(Response.Status.OK);
+//        return builder.build();
+//    }
+
+
+//    @POST
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    public Response addMarketdataEntity(String jsonBody)
+//            throws GenericEntityException, ConversionException {
+//        String entityName = "MarketdataModel";
+//        Response.ResponseBuilder builder;
+//        Delegator delegator = (Delegator) servletContext.getAttribute("delegator");
+//        GenericValue object = jsonToGenericConverter.convert(delegator.getDelegatorName(), entityName,
+//                JSON.from(jsonBody));
+//        ModelEntity model = delegator.getModelEntity(entityName);
+//        if (model.getPksSize() == 1) {
+//            // because can only sequence if one PK field
+//            if (object.get(model.getFirstPkFieldName()) == null) {
+//                object.setNextSeqId();
+//            }
+//        }
+//
+//
+//        delegator.create(object); // TODO: catch exception
+//        builder = Response.status(Response.Status.OK);
+//        return builder.build();
+//    }
 
 }
