36208a87143c2e86c48cffb422db2e03caf740aa
Rihard <rihard.liiva@gmail.com>
2020-04-14 22:30:15 +0300

    change service

--- plugins/ofbizpublisher/src/main/java/ee/ttu/ofbizpublisher/services/jersey/ObjectDistService.java
+++ plugins/ofbizpublisher/src/main/java/ee/ttu/ofbizpublisher/services/jersey/ObjectDistService.java
@@ -40,59 +40,59 @@ public class ObjectDistService {
         Response.ResponseBuilder builder = null;
         LocalDispatcher dispatcher = (LocalDispatcher) servletContext.getAttribute("dispatcher");
         DispatchContext dpc = dispatcher.getDispatchContext();
-        builder = Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(dpc.getAllServiceNames());
+        builder = Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity("Hello world");
         return builder.build();
     }
 
-    @GET
-    @Path("/{name}")
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response getServiceDetails(@PathParam(value = "name") String serviceName) throws GenericServiceException {
-        Response.ResponseBuilder builder = null;
-        LocalDispatcher dispatcher = (LocalDispatcher) servletContext.getAttribute("dispatcher");
-        DispatchContext dpc = dispatcher.getDispatchContext();
-        Object obj = dpc.getModelService(serviceName).getModelParamList();
-        builder = Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(obj);
-        return builder.build();
-    }
-
-    @POST
-    @Path("/{name}")
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Produces(MediaType.APPLICATION_JSON)
-    public Response callService(@PathParam(value = "name") String serviceName, String jsonBody) throws GenericServiceException, IOException {
-        Response.ResponseBuilder builder = null;
-        LocalDispatcher dispatcher = (LocalDispatcher) servletContext.getAttribute("dispatcher");
-        Delegator delegator = (Delegator) servletContext.getAttribute("delegator");
-
-        JSON body = JSON.from(jsonBody);
-
-        Map<String, Object> fieldMap;
-
-        fieldMap = UtilGenerics.<Map<String, Object>>cast(body.toObject(Map.class));
-
-        for (String key : fieldMap.keySet()) {
-            Object obj = fieldMap.get(key);
-            try {
-                Map<String, Object> test = UtilGenerics.<Map<String, Object>>cast(JSON.from(obj).toObject(Map.class));
-                if (test.containsKey("_ENTITY_NAME_")) {
-                    fieldMap.put(key, jsonToGenericConverter.convert(delegator.getDelegatorName(), JSON.from(obj)));
-                } else {
-                    fieldMap.put(key, test);
-                }
-            } catch (IOException | ConversionException ignored) {
-            }
-        }
-        try {
-            Map<String, ?> entity = dispatcher.runSync(serviceName, fieldMap);
-            builder = Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(entity);
-        } catch (GenericServiceException e)  {
-            e.printStackTrace();
-            Map<String, Object> errors = new HashMap<>();
-            errors.put("Error", e.getMessage());
-            builder = Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON).entity(errors);
-        }
-        return builder.build();
-    }
+//    @GET
+//    @Path("/{name}")
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response getServiceDetails(@PathParam(value = "name") String serviceName) throws GenericServiceException {
+//        Response.ResponseBuilder builder = null;
+//        LocalDispatcher dispatcher = (LocalDispatcher) servletContext.getAttribute("dispatcher");
+//        DispatchContext dpc = dispatcher.getDispatchContext();
+//        Object obj = dpc.getModelService(serviceName).getModelParamList();
+//        builder = Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(obj);
+//        return builder.build();
+//    }
+//
+//    @POST
+//    @Path("/{name}")
+//    @Consumes(MediaType.APPLICATION_JSON)
+//    @Produces(MediaType.APPLICATION_JSON)
+//    public Response callService(@PathParam(value = "name") String serviceName, String jsonBody) throws GenericServiceException, IOException {
+//        Response.ResponseBuilder builder = null;
+//        LocalDispatcher dispatcher = (LocalDispatcher) servletContext.getAttribute("dispatcher");
+//        Delegator delegator = (Delegator) servletContext.getAttribute("delegator");
+//
+//        JSON body = JSON.from(jsonBody);
+//
+//        Map<String, Object> fieldMap;
+//
+//        fieldMap = UtilGenerics.<Map<String, Object>>cast(body.toObject(Map.class));
+//
+//        for (String key : fieldMap.keySet()) {
+//            Object obj = fieldMap.get(key);
+//            try {
+//                Map<String, Object> test = UtilGenerics.<Map<String, Object>>cast(JSON.from(obj).toObject(Map.class));
+//                if (test.containsKey("_ENTITY_NAME_")) {
+//                    fieldMap.put(key, jsonToGenericConverter.convert(delegator.getDelegatorName(), JSON.from(obj)));
+//                } else {
+//                    fieldMap.put(key, test);
+//                }
+//            } catch (IOException | ConversionException ignored) {
+//            }
+//        }
+//        try {
+//            Map<String, ?> entity = dispatcher.runSync(serviceName, fieldMap);
+//            builder = Response.status(Response.Status.OK).type(MediaType.APPLICATION_JSON).entity(entity);
+//        } catch (GenericServiceException e)  {
+//            e.printStackTrace();
+//            Map<String, Object> errors = new HashMap<>();
+//            errors.put("Error", e.getMessage());
+//            builder = Response.status(Response.Status.BAD_REQUEST).type(MediaType.APPLICATION_JSON).entity(errors);
+//        }
+//        return builder.build();
+//    }
 
 }
