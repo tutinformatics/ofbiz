43b46abf81225e9b345f3e65ebc5f7437f8a2b7c
Mihail-PC <mismir@ttu.ee>
2020-04-25 11:08:32 +0300

    build fixes

--- deploy-dev-light.sh
+++ deploy-dev-light.sh
@@ -1,5 +1,6 @@
 sudo docker image rmi $(sudo docker image ls -f='dangling=true' -q) -f || true
 docker-compose -p ofbiz -f docker-compose-databases-dev-light.yml up -d
+docker-compose -f docker-compose-databases.yml restart master
 docker-compose -p ofbiz -f docker-compose-ofbiz.yml build
 docker-compose -p ofbiz -f docker-compose-ofbiz.yml up
 sudo docker image rmi $(sudo docker image ls -f='dangling=true' -q) -f || true
