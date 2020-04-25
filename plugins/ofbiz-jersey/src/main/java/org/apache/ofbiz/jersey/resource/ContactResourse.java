dfd441b30da5f919c761c37588188d4aa1f0ac8e
fops <sawka1997@gmail.com>
2020-04-25 22:00:52 +0300

    deleted contact

--- plugins/ofbiz-jersey/src/main/java/org/apache/ofbiz/jersey/resource/ContactResourse.java
+++ plugins/ofbiz-jersey/src/main/java/org/apache/ofbiz/jersey/resource/ContactResourse.java
@@ -1,10 +1,8 @@
 package org.apache.ofbiz.jersey.resource;
 
 
-import org.apache.ofbiz.entity.GenericEntityException;
-import org.apache.ofbiz.jersey.contact.ContactDTO;
-import org.apache.ofbiz.jersey.contact.ContactService;
 
+import org.apache.ofbiz.entity.GenericEntityException;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.GET;
@@ -14,7 +12,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.Provider;
-import java.util.List;
 
 
 @Path("/contacts")
@@ -29,7 +26,7 @@ public class ContactResourse {
 
     @Context
     private ServletContext servletContext;
-    private ContactService contactService;
+
 
 
 
@@ -37,14 +34,15 @@ public class ContactResourse {
     @Produces(MediaType.APPLICATION_JSON)
     public Response getContacts() throws GenericEntityException {
 
-        List<ContactDTO> entity = contactService.getContacts();
-
-        System.out.println(entity);
-        Response.ResponseBuilder builder = Response.
-                status(Response.Status.OK)
-                .type(MediaType.APPLICATION_JSON)
-                .entity(entity);
-        return builder.build();
+//        String entity;
+//
+//        System.out.println(entity);
+//        Response.ResponseBuilder builder = Response.
+//                status(Response.Status.OK)
+//                .type(MediaType.APPLICATION_JSON)
+//                .entity(entity);
+//        return builder.build();
+        return null;
     }
 
 
