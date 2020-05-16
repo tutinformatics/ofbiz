628cc9259dec68bd252424ab76aa63e8c0adf795
Mihail-PC <mismir@ttu.ee>
2020-05-16 22:27:23 +0300

    aff_marketing extended docs

--- plugins/aff_marketing/docs/deploy.sh
+++
@@ -1,4 +0,0 @@
-version=v15
-sudo docker build -t gcr.io/uplifted-cinema-271812/ofbiz-back:$version .
-sudo docker push gcr.io/uplifted-cinema-271812/ofbiz-back:$version
-sudo kubectl set image deployment/ofbiz-back ofbiz-back=gcr.io/uplifted-cinema-271812/ofbiz-back:$version
