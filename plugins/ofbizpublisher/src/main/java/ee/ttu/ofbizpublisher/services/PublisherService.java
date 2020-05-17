4b7458c2e76367fb478ed3381cd35a10f6c77101
Robert <roaltm@ttu.ee>
2020-05-17 14:56:03 +0300

    Added sleep

--- plugins/ofbizpublisher/src/main/java/ee/ttu/ofbizpublisher/services/PublisherService.java
+++ plugins/ofbizpublisher/src/main/java/ee/ttu/ofbizpublisher/services/PublisherService.java
@@ -91,6 +91,7 @@ public class PublisherService {
     }
 
     public void setPublisherDataWithPublisher(String entityName, String topic, String filterParams) throws Exception {
+        Thread.sleep(3000);
         List<GenericValue> genericValues = findFilteredEntities(entityName, topic, filterParams);
         publisher.setTopic(topic);
         publisher.call(genericValues);
